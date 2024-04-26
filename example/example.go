5134a7035d5d338a5d114268233a0925f594596f
ACDEV <andresdavidcm24@gmail.com>
2024-04-26 11:16:55 -0500

    Fixing package naming

---
+++ example/example.go
@@ -0,0 +1,14 @@
+package logger
+
+import "github.com/adcmdev/logger"
+
+func main() {
+	logger.Init(DebugLevel)
+
+	logger.Log.Debug("Debug message")
+	logger.Log.Info("Info message")
+	logger.Log.Warning("Warning message")
+	logger.Log.Error("Error message")
+	// logger.Log.Critical("Critical message")
+	// logger.Log.Fatal("Fatal message")
+}
---
+++ example/go.mod
@@ -0,0 +1,10 @@
+module example
+
+go 1.22.2
+
+require github.com/adcmdev/logger v1.3.0
+
+require (
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
